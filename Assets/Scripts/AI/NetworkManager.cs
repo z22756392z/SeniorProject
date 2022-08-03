1be9f2fc45c972fbfbcedb72b577330946134097
stereomp3 <ericjjkkhandsome@gmail.com>
2022-08-03 18:36:41 +0800

    add return string func

--- Assets/Scripts/AI/NetworkManager.cs
+++ Assets/Scripts/AI/NetworkManager.cs
@@ -4,29 +4,33 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
 
 /// <summary>
 /// This class handles all the requests and serialization and
 /// deserialization of data.
 /// </summary>
-public class NetworkManager : MonoBehaviour {
+public class NetworkManager : MonoBehaviour
+{
     // reference to the BotUI class
-    public BotUI            botUI;
+    public BotUI botUI;
     // the url at which the bot's custom component is hosted
-    private const string    rasa_url = "http://127.0.0.1:5005/webhooks/unity/webhook";
-    
+    private const string rasa_url = "http://127.0.0.1:5005/webhooks/unity/webhook";
+
     /// <summary>
     /// This method is called when user has entered their message and hits
     /// the send button. It calls the <see cref="NetworkManager.PostRequest"/> coroutine
     /// to send the user message to bot and also updates UI with the users message.
     /// </summary>
-    public void SendMessage () {
+    public void SendMessage()
+    {
         // Get message from textbox and clear the input field
         string message = botUI.input.text;
         botUI.input.text = "";
 
         // Create a json object from user message
-        PostData postMessage = new PostData {
+        PostData postMessage = new PostData
+        {
             sender = "doku",
             message = message
         };
@@ -44,24 +48,32 @@ public class NetworkManager : MonoBehaviour {
     /// This method updates the UI with the bot's response.
     /// </summary>
     /// <param name="response">The response json recieved from the bot</param>
-    public void RecieveMessage (string response) {
+    public void RecieveMessage(string response)
+    {
         // Deserialize response recieved from the bot
-        RootMessages recieveMessages = 
+        RootMessages recieveMessages =
             JsonUtility.FromJson<RootMessages>("{\"messages\":" + response + "}");
 
+        Debug.Log(ConvertJsonStringToSting(response));
+
         // show message based on message type on UI
-        foreach (RecieveData message in recieveMessages.messages) {
+        foreach (RecieveData message in recieveMessages.messages)
+        {
             FieldInfo[] fields = typeof(RecieveData).GetFields();
-            foreach (FieldInfo field in fields) {
+            foreach (FieldInfo field in fields)
+            {
                 string data = null;
-                
+
                 // extract data from response in try-catch for handling null exceptions
-                try {
+                try
+                {
                     data = field.GetValue(message).ToString();
-                } catch (NullReferenceException) {}
-                
+                }
+                catch (NullReferenceException) { }
+
                 // print data
-                if (data != null && field.Name != "recipient_id") {
+                if (data != null && field.Name != "recipient_id")
+                {
                     botUI.UpdateDisplay("Bot", data, field.Name);
                 }
             }
@@ -75,7 +87,8 @@ public class NetworkManager : MonoBehaviour {
     /// <param name="url">The url where the rasa server's custom connector is located</param>
     /// <param name="jsonBody">User message serialized into a json object</param>
     /// <returns></returns>
-    private IEnumerator PostRequest (string url, string jsonBody) {
+    private IEnumerator PostRequest(string url, string jsonBody)
+    {
         // Create a request to hit the rasa custom connector
         UnityWebRequest request = new UnityWebRequest(url, "POST");
         byte[] rawBody = new System.Text.UTF8Encoding().GetBytes(jsonBody);
@@ -106,48 +119,64 @@ public class NetworkManager : MonoBehaviour {
     /// <param name="url">url where the image resource is located</param>
     /// <param name="image">RawImage object on which the texture will be applied</param>
     /// <returns></returns>
-    public IEnumerator SetImageTextureFromUrl (string url, Image image) {
+    public IEnumerator SetImageTextureFromUrl(string url, Image image)
+    {
         // Send request to get the image resource
         UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
         yield return request.SendWebRequest();
-        
+
         if (request.isNetworkError || request.isHttpError)
             // image could not be retrieved
             Debug.Log(request.error);
-        
-        else {
+
+        else
+        {
             // Create Texture2D from Texture object
             Texture texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
             Texture2D texture2D = texture.ToTexture2D();
 
             // set max size for image width and height based on chat size limits
             float imageWidth = 0, imageHeight = 0, texWidth = texture2D.width, texHeight = texture2D.height;
-            if ((texture2D.width > texture2D.height) && texHeight > 0) {
+            if ((texture2D.width > texture2D.height) && texHeight > 0)
+            {
                 // Landscape image
                 imageWidth = texWidth;
                 if (imageWidth > 200) imageWidth = 200;
                 float ratio = texWidth / imageWidth;
                 imageHeight = texHeight / ratio;
             }
-            if ((texture2D.width < texture2D.height) && texWidth > 0) {
+            if ((texture2D.width < texture2D.height) && texWidth > 0)
+            {
                 // Portrait image
                 imageHeight = texHeight;
                 if (imageHeight > 200) imageHeight = 200;
                 float ratio = texHeight / imageHeight;
-                imageWidth = texWidth/ ratio;
+                imageWidth = texWidth / ratio;
             }
 
             // Resize texture to chat size limits and attach to message 
             // Image object as sprite
             TextureScale.Bilinear(texture2D, (int)imageWidth, (int)imageHeight);
             image.sprite = Sprite.Create(
-                texture2D, 
-                new Rect(0.0f, 0.0f, texture2D.width, texture2D.height), 
+                texture2D,
+                new Rect(0.0f, 0.0f, texture2D.width, texture2D.height),
                 new Vector2(0.5f, 0.5f), 100.0f);
 
             // Resize and reposition all chat bubbles
             StartCoroutine(botUI.RefreshChatBubblePosition());
         }
+    }
+
+    private string ConvertJsonStringToSting(string json_string)
+    {
+        json_string.Replace("[", "'").Replace("]", "'");
+        JObject json = JObject.Parse(json_string.Replace("]", "").Replace("[", ""));
+        foreach (var e in json)
+        {
+            Debug.Log(e);
+            if (e.Key == "text") return e.Value.ToString();
+        }
+        return json_string;
     }
 
 }
